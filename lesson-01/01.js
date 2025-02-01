7424e0b1ca04c04125476c3db4263c5c90e93655
Dmitrii599 <osipowy@yandex.ru>
2025-02-01 20:15:26 +0400

    complete task-9

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -2,4 +2,11 @@
 // Результат присвойте переменной sum
 
 // your code
-let sum
+let sum = 0;
+let currentNumber = 1;
+
+while (number <=20) {
+    sum += currentNumber;
+    currentNumber++;
+}
+console.log("Сумма чисел от 1 до 20:", sum);
