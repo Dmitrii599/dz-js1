66cd3be86cd3774c1683318e47963f8586d964ab
Dmitrii599 <osipowy@yandex.ru>
2025-02-16 13:42:10 +0400

    first

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,9 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(first, second) {
+    const result = ((first + second) / calculateAverage.length);
+    // console.log(result)
+    return result;
+}
+calculateAverage(4, 12);
+console.log(calculateAverage(4, 12))
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,11 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(first, second, third) {
+    const comparison = Math.max(first, second, third);
+    return comparison;
+}
+findLargest(2, 4, 7);
+findLargest(3, 9, 8);
+
+console.log(findLargest(2, 4, 7))
+console.log(findLargest(3, 9, 8))
