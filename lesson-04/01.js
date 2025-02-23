15fc9e01ca35ae1bcb2643f804070b1b498025ef
Dmitrii599 <osipowy@yandex.ru>
2025-02-23 13:59:45 +0400

    first

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,22 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array, element) {
+
+    for (let i = 0; i < array.length; i++) 
+        if (array[i] === element) {
+        return true
+    }
+    return false
+}
+
+includesElement([1, 2, 3], 2);
+// let array = [1, 2, 3];
+// let element = 4;
+console.log(includesElement([1, 2, 3], 2));
+
+// includesElement([4, 5, 6], 7);
+// let arrays = [4, 5, 6];
+// let elements = 6;
+// console.log(includesElement([4, 5, 6], 7));
+
